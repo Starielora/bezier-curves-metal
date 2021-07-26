269650c15accb91922372e226192b90ba2ee0c7f
Starielora <edyko.patryk@gmail.com>
2021-07-26 22:16:53 +0200

    Initial Commit

---
+++ Shared/Bezier3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bezier3DApp.swift
+//  Shared
+//
+//  Created by Patryk Edyko on 26/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bezier3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
