e60761f3c23b6c083a1c69ceb0dc0309ef350742
Starielora <edyko.patryk@gmail.com>
2021-08-02 00:38:02 +0200

    Replaced MDLCamera workaround in Line class.
    Signed-off-by: Patryk Edyko <patryk.edyko@icloud.com>

--- Shared/Line.swift
+++ Shared/Line.swift
@@ -11,22 +11,25 @@ import MetalKit
 class Line : Model {
     
     private let radii: Float = 0.01
-    private let camera: MDLCamera // TODO implement rotation properly
     
     init(device: MTLDevice, library: MTLLibrary)
     {
         let allocator = MTKMeshBufferAllocator(device: device)
         let mdlMesh = MDLMesh.newCylinder(withHeight: 1, radii: [radii, radii], radialSegments: 10, verticalSegments: 1, geometryType: .triangles, inwardNormals: false, allocator: allocator)
-        camera = MDLCamera()
         super.init(mdlMesh: mdlMesh, device: device, library: library)
     }
     
+    var nanos: [UInt64] = []
+    
     func move(p1: SIMD3<Float>, p2: SIMD3<Float>)
     {
-        let d = sqrt( pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2) + pow(p2.z - p1.z, 2) ) // TODO is already implemented in SIMD?
-        camera.look(at: p1, from: ((p1+p2)/2))
-        mdlMesh.transform = camera.transform
-        transform.scale.y = d
-        transform.rotation.x += Float.pi/2
+        let lineLength = simd_distance(p1, p2)
+        let translationVector = SIMD4<Float>((p2+p1)/2, 1)
+        let lineDirection = simd_normalize(p2 - p1)
+
+        let translation = simd_float4x4([1,0,0,0], [0,1,0,0], [0,0,1,0], translationVector)
+        let scale = simd_float4x4([1,0,0,0], [0,lineLength,0,0], [0,0,1,0], [0,0,0,1])
+        let rotation = simd_float4x4(simd_quatf(from: [0,1,0], to: lineDirection))
+        transform.matrix = translation * rotation * scale
     }
 }
